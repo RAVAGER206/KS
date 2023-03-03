2a9b91efcd35f652cad8dea49752d26f6f1b855e
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2023-03-03 09:37:08 +0300

    Add files via upload

---
+++ sort.h
@@ -0,0 +1,30 @@
+#ifndef SORT_H_INCLUDED
+#define SORT_H_INCLUDED
+
+
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+
+////////////////////////-FUNCTION.AHNIK-////////////////////////
+
+void sa (vector <int> &mass);
+
+void ra(vector <int>& mass);
+
+void rra(vector <int>& mass);
+
+void pb (vector <int> &mass, vector <int> &mass1);
+
+void zn(const vector <int>& cop, vector <int>& osn);
+
+int mp (string str);
+
+int mn (vector <int> mass);
+
+
+/////////////////////////////-END-/////////////////////////////
+
+#endif
